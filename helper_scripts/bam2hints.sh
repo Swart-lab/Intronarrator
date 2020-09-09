6496f4707524aeb9cc72c0550804cd5c58e9e307
Estienne Swart <estienne.swart@tuebingen.mpg.de>
2020-09-09 14:55:19 +0200

    Cleaned up intronarrator main shell script and fixed typos.

--- helper_scripts/bam2hints.sh
+++ helper_scripts/bam2hints.sh
@@ -6,12 +6,12 @@ BAM_PREFIX=assembly.fixed.minus_mt.mapped_only
 FWD_BAM=$BAM_PREFIX.fwd.bam
 REV_BAM=$BAM_PREFIX.rev.bam
 
-#$BAM2WIG $FWD_BAM > $BAM_PREFIX.fwd.wig
+$BAM2WIG $FWD_BAM > $BAM_PREFIX.fwd.wig
 
 $WIG2HINTS --width=10 --margin=10 --minthresh=2 --minscore=4 --prune=0.1 --src=W --type=ep --width=10 --margin=10 --minthresh=2 --minscore=4 --prune=0.1 --src=W --type=ep --pri=2 \
 --strand=+ --radius=4.5 < $BAM_PREFIX.fwd.wig > $BAM_PREFIX.fwd.eph.gff
 
-#$BAM2WIG $REV_BAM > $BAM_PREFIX.rev.wig
+$BAM2WIG $REV_BAM > $BAM_PREFIX.rev.wig
 
 $WIG2HINTS --width=10 --margin=10 --minthresh=2 --minscore=4 --prune=0.1 --src=W --type=ep --width=10 --margin=10 --minthresh=2 --minscore=4 --prune=0.1 --src=W --type=ep --pri=2 \
 --strand=- --radius=4.5 < $BAM_PREFIX.rev.wig > $BAM_PREFIX.rev.eph.gff
--- intronarrator.sh
+++ intronarrator.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 #add directory containing intronnarator scripts
-INTRONARRATOR_PATH=/ebio/ag-swart/home/eswart/ag-swart-blepharisma/data/alignments/intron_searches/intronarrator
+INTRONARRATOR_PATH=/ebio/abt2_projects/ag-swart-blepharisma/development/intronarrator
 export PATH=$PATH:$INTRONARRATOR_PATH:$INTRONARRATOR_PATH/ncRNA_searches
+
+# --- Key file names/prefixes of genome assembly, RNA-seq BAM and AUGUSTUS hints:
+ASM=assembly.fixed.minus_mt #Genome assembly prefix without ".fa" extension
+BAM=assembly.fixed.minus_mt.mapped_only.bam #BAM file containing all mapped RNA-seq reads
+EP_hints=assembly.fixed.minus_mt.mapped_only.eph #AUGUSTUS hints file without ".gff" extension
+#The hints file above can be created with the scripts in "helper_scripts"
+
+RFAM_DB=/ebio/abt2_projects/ag-swart-blepharisma/db/Rfam/rfam_minimal.cm #Minimal RFAM database
 
 # --- Intron splicing parameters:
 MIN_INTRON_RATIO=0.2 #default 0.3 - at least 30% of mapped reads with introns
@@ -12,34 +20,24 @@ MAX_INTRON_LEN=40 #default 40 - only find introns less than this length (bp)
 ALL_PROCS=32 #number of processes for multiprocessing/threading
 INFERNAL_PROCS=$ALL_PROCS #number of processes for Infernal searches of ncRNAs
 PYSAM_PROCS=$ALL_PROCS #number of processes for intron splicing counting
-BAM=assembly.fixed.minus_mt.mapped_only.bam
-#/tmp/scratch/map/rna-seq/merged.intron_lines.sorted.bam
-
-ASM=assembly.fixed.minus_mt #Genome assembly prefix without ".fa" extension
 
 # --- AUGUSTUS binary, config and exon part hints paths/config
-AUGUSTUS_CONFIG_PATH=/ebio/ag-swart/home/eswart/ag-swart-blepharisma/data/gene_prediction/augustus/augustus_mod_gencode2_ps_bjap_v2/config
-AUGUSTUS_SCRIPTS=/ebio/abt2_projects/ag-swart-blepharisma/build/Augustus/scripts/
+AUGUSTUS_CONFIG_PATH=/ebio/abt2_projects/ag-swart-blepharisma/data/gene_prediction/augustus/augustus_mod_gencode2_ps_bjap_v2/config
 export AUGUSTUS_CONFIG_PATH
+AUGUSTUS_SCRIPTS=/ebio/abt2_projects/ag-swart-blepharisma/build/Augustus/scripts/
 AUGUSTUS_BIN=/ebio/abt2_projects/ag-swart-blepharisma/build/Augustus/bin/augustus
-#_shorty_2
 AUGUSTUS_EXTRINSIC=$AUGUSTUS_CONFIG_PATH/species/bjap_v26/bjap_v26_extrinsic.M.RM.E.W.cfg
 
-#The hints below for AUGUSTUS can be created with the scripts in "helper_scripts"
-EP_hints=assembly.fixed.minus_mt.mapped_only.eph #AUGUSTUS hints file without ".gff" extension
+# --- File splitting for AUGUSTUS:
 SPLIT_FILE_SIZE=2000000 #Size of chunks to try to split genome into for parallel AUGUSTUS gene prediction
                         #This will automatically set the number of processes executed to the number of chunks
                         #So, a large enough number should be chosen to avoid forking too many processes
 SPLIT_GENOME=split_genome
 SPLIT_GFF=split_gff
 
-JUNCS=$ASM.juncs
-
-RFAM_DB=/ebio/ag-swart/home/eswart/ag-swart-blepharisma/db/Rfam/Rfam.cm
-
-# --- Filename setup - It should not be necessary to change anything in this
-# section.
+# --- Filename setup - It should not be necessary to change anything in this section.
 REALTRONS_PREFIX=all.realtrons
+JUNCS=$ASM.juncs
 REALTRONS_PREFIX2=$REALTRONS_PREFIX.${MIN_INTRON_RATIO}
 REALTRONS_FN=$REALTRONS_PREFIX2.gff
 REALTRONS_NO_ALT=all.realtrons.${MIN_INTRON_RATIO}.noalt.gff
@@ -47,37 +45,42 @@ ASM_M=$ASM.${MIN_INTRON_RATIO}
 ASM_ncRNA_masked=$ASM_M.minus_introns.ncRNA_hard_masked
 EP_hints_minus=$EP_hints.$MIN_INTRON_RATIO
 
-# --- Directory setup - It should not be necessary to change anything in this
+# --- Directory setup - It should not be necessary to change in this section.
 mkdir -p $SPLIT_GENOME
 mkdir -p $SPLIT_GFF
 mkdir -p tmp
 mkdir -p tmp_juncs
 
-# --- Code execution below ---
+# --- Intronarrator pipeline ---
 
+##find introns
 pysam_extract_introns_parallel.py --processes $PYSAM_PROCS --bam $BAM --genome $ASM.fa --outfile $JUNCS
 sort -k 1,1 -k 2,2n $JUNCS > $JUNCS.sorted
 
+##count both spliced reads and total reads properly 
 pysam_count_reads.py --processes $PYSAM_PROCS --bam $BAM --intron_juncs $JUNCS.sorted --genome $ASM.fa --outfile $ASM.intron_counts.txt 
-count both spliced reads and total reads properly 
 
+##identify "real" introns
 realtrons.py $ASM.intron_counts.txt 0.01 $MIN_INTRONS $MAX_INTRON_LEN > $REALTRONS_PREFIX.0.01.gff
 antisense_realtrons.py $ASM.intron_counts.txt 0.01 $MIN_INTRONS $MAX_INTRON_LEN > $REALTRONS_PREFIX.0.01.antisense.gff
 
 realtrons.py $ASM.intron_counts.txt $MIN_INTRON_RATIO $MIN_INTRONS $MAX_INTRON_LEN > $REALTRONS_FN
 antisense_realtrons.py $ASM.intron_counts.txt 0.01 $MIN_INTRONS $MAX_INTRON_LEN > $REALTRONS_PREFIX2.antisense.gff
 
+##Select only the most frequently spliced introns that share a boundary
 realtrons_filter_alt.py $REALTRONS_FN > $REALTRONS_NO_ALT
-#Selects only the most frequently spliced introns that share a boundary
 realtrons_filter_alt.py $REALTRONS_PREFIX2.antisense.gff > $REALTRONS_NO_ALT.antisense
 
+##Mask and remove introns to create an intron- genome
 mask_introns.py $ASM.fa $REALTRONS_NO_ALT >  $ASM_M.intron_masked.fa
 remove_introns.py $ASM_M.intron_masked.fa > $ASM_M.minus_introns.fa
 
+##Convert AUGUSTUS exon part coordinates into the corresponding intron- genome ones
 convert_ep_hint_coords.py $ASM_M.intron_masked.fa $EP_hints.gff 1> $EP_hints_minus.gff 2> $EP_hints.gff.err
 
-#ncRNA search with Infernal, followed by filtering to remove undesirable domains and weak matches:
-cmsearch --cpu $INFERNAL_PROCS --tblout $ASM_M.minus_introns.cmsearch $RFAM_DB $ASM.minus_introns.fa > $ASM.minus_introns.cmsearch.aln
+##ncRNA search with Infernal, followed by filtering to remove undesirable domains and weak matches:
+echo cmsearch running using $RFAM_DB
+cmsearch --cpu $INFERNAL_PROCS --tblout $ASM_M.minus_introns.cmsearch $RFAM_DB $ASM_M.minus_introns.fa > $ASM_M.minus_introns.cmsearch.aln
 infernal_filter.py $ASM_M.minus_introns.cmsearch > $ASM_M.minus_introns.filtered.cmsearch
 infernal_hard_masker.py $ASM_M.minus_introns.filtered.cmsearch $ASM_M.minus_introns.fa > $ASM_ncRNA_masked.fa
 
@@ -100,7 +103,7 @@ do
 done
 
 FAIL=0
-#Wait on execution of all the AUGUSTUS processes before continuing
+##Wait on execution of all the AUGUSTUS processes before continuing
 for pid in ${pids[*]}; do
     wait $pid || let "FAIL+=1"
 done
@@ -112,6 +115,7 @@ else
   echo "($FAIL) AUGUSTUS jobs failed!"
 fi
 
+##Combine AUGUSTUS files and fix the gene names
 merge_and_fix_AUGUSTUS_gene_names.py "$SPLIT_GFF/*" > $ASM_ncRNA_masked.gff 
 
 add_introns_back_to_AUGUSTUS_gff.py $ASM_M.intron_masked.fa $ASM_ncRNA_masked.gff $REALTRONS_NO_ALT > $ASM_M.final.gff 
