b75ddd31544f06492b241f1d9d504667e82a1dcd
Estienne Swart <estienne.swart@tuebingen.mpg.de>
2020-10-21 16:44:59 +0200

    Further fixes to infernal gff conversion for EMBLmyGFF3

--- ncRNA_searches/infernal_to_GFF.py
+++ ncRNA_searches/infernal_to_GFF.py
@@ -31,20 +31,22 @@ for line in open(argv[1]).readlines():
     continue
 
   ncRNA_kind = 'ncRNA'
+  extra_id = ''
+
   if "rRNA" in kind:
     ncRNA_kind = 'rRNA'
   elif kind[0] == "U":
-    ncRNA_kind = 'snRNA'
+    extra_id = ';ncRNA_class="snRNA"'
   elif 'SRP' in kind:
-    ncRNA_kind = 'SRP_RNA'
+    extra_id =  ';ncRNA_class="SRP_RNA"'
   elif 'MRP' in kind:
-    ncRNA_kind = 'RNase_MRP_RNA'
+    extra_id = ';ncRNA_class="RNase_MRP_RNA"'
   elif 'RNaseP' in kind:
-    ncRNA_kind = 'RNase_P_RNA'
+    extra_id = ';ncRNA_class="RNase_P_RNA"'
   
 
-  print("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\tID=%s_gene" % 
-    (contig, prog, "gene", start, end, score, strand, phase, gene_id))
-  print("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\tID=%s" % 
-    (contig, prog, ncRNA_kind, start, end, score, strand, phase, gene_id))
+  print("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\tID=%s_gene%s" % 
+    (contig, prog, "gene", start, end, score, strand, phase, gene_id, extra_id))
+  print("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\tID=%s%s" % 
+    (contig, prog, ncRNA_kind, start, end, score, strand, phase, gene_id, extra_id))
 
