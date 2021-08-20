31d2190a8af6d1b6971de2b58996454b9d2deac7
gh-ecs <estienne.swart@gmail.com>
2021-08-20 15:48:51 +0200

    Update infernal_filter.py

--- ncRNA_searches/infernal_filter.py
+++ ncRNA_searches/infernal_filter.py
@@ -2,11 +2,6 @@
 from Bio import SeqIO
 from sys import argv, stderr
 
-#f = SeqIO.parse(open(argv[1]), "fasta")
-
-#for rec in f:
-#  print(">%s\n%s" % (rec.name, rec.seq.translate(table=4)))
-
 not_allowed = ['bacteria', 'archaea', 'microsporidia', 'Plant', 'ACEA',
 'etazoa', 'ictyostelium', 'RMST_9']
 
